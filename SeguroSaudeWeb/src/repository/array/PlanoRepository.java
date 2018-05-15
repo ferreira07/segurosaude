323c96bc3d17c77d11955592dcabe7c02c612aa8
Thiago Santos <thiago.f.b.santos@accenture.com>
2018-05-15 00:10:49 -0300

    Retirando arquivos do repositorio em lista

--- SeguroSaudeWeb/src/repository/array/PlanoRepository.java
+++
@@ -1,8 +0,0 @@
-package repository.array;
-
-import entities.Plano;
-import repository.IPlanoRepository;
-
-public class PlanoRepository extends AbstractRepository<Plano> implements IPlanoRepository{
-
-}
